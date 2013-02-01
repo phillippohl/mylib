c9f1651b8f8039700d49cd106e692ea854399a8f
phillippohl <phillipp-ohl@web.de>
2013-02-01 22:36:14 +0100

    Changed class name

--- src/log/FileLogger.java
+++ src/log/FileLogger.java
@@ -14,27 +14,29 @@ import java.io.PrintWriter;
  * @author Phillipp
  * @version 0.2
  */
-public class MessageLog {
+public class FileLogger {
 	
 	private String FILENAME;
 	private File log;
 	private BufferedReader br;
 	private PrintWriter pw;
 	/**
-	 * 
+	 * Default constructor
 	 */
-	public MessageLog() {
+	public FileLogger() {
 		this.FILENAME = "logfile.log";
 		this.log = new File(FILENAME);
 	}
 	/**
-	 * @param filename If provided, the filename for the log file
+	 * @param filename if provided, the filename for the log file
 	 */
-	public MessageLog(String filename) {
+	public FileLogger(String filename) {
 		this.FILENAME = filename;
 		this.log = new File(FILENAME);
 	}
-	
+	/**
+	 * @return current entries from log file
+	 */
 	private String readFromLog() {
 		String input = "";
 		System.out.println("Reading from " + FILENAME + "...");
@@ -50,7 +52,9 @@ public class MessageLog {
 		System.out.println("Reading finished...");
 		return input;
 	}
-	
+	/**
+	 * @return check value if operation executed successfully
+	 */
 	public int writeToLog(String message) {
 		String input = this.readFromLog();
 		System.out.println("Writing to " + FILENAME + "...");
@@ -69,7 +73,7 @@ public class MessageLog {
 	}
 	
 	public static void main(String[] args){
-		MessageLog m = new MessageLog();
+		FileLogger m = new FileLogger();
 		m.writeToLog("Hello World!");
 	}
 }
